e0a8af9829656cc149f3a2dda42005ea759214fa
Mohankumar R <mohan2609kumar@gmail.com>
2023-09-22 08:11:33 +0530

    travel-website-basic-frontend added

---
+++ TopSpeed.Web/Controllers/TraviaController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TopSpeed.Web.Models;
+
+namespace TopSpeed.Web.Controllers
+{
+    public class TraviaController : Controller
+    {
+        private readonly ILogger<TraviaController> _logger;
+
+        public TraviaController(ILogger<TraviaController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Destinations()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TopSpeed.Web/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+let navbar = document.querySelector('.header .flex .navbar');
+
+document.querySelector('#menu-btn').onclick = () => {
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    navbar.classList.remove('active');
+}
+
+document.querySelectorAll('input[type="number"]').forEach(inputNumber => {
+    inputNumber.oninput = () => {
+        if (inputNumber.value.length > inputNumber.maxLength) inputNumber.value = inputNumber.value.slice(0, inputNumber.maxLength);
+    };
+});
+
+AOS.init({
+    duration: 400,
+    delay: 200,
+});
